7ac0c4fa269fef4e967bac4530bd78bc781efe76
danielcsgomes <me@danielcsgomes.com>
2012-08-20 19:31:39 +0100

    removed unnecessary files

--- AngularJS-NetPonto/App_Start/RouteConfig.cs
+++ AngularJS-NetPonto/App_Start/RouteConfig.cs
@@ -14,16 +14,10 @@ namespace AngularJS_NetPonto
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "API",
+                url: "api/{controller}/{action}/{id}",
+                defaults: new { id = UrlParameter.Optional }
             );
         }
     }
